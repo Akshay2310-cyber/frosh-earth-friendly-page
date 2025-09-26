b4f9112ff20170e719f04284e15e33ed7815a6e6
Akshay2310-cyber <ms.akshay.2310@gmail.com>
2025-09-26 10:47:37 +0530

    Fix React Router for GitHub Pages - add basename and 404 handling

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/frosh-earth-friendly-page">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
